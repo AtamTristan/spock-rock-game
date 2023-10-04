1ebb1de764ad448567886e7bb221ebdc75a21934
tristanwesendahlvelazquez <tristan.wesendahl@hotmail.com>
2023-10-04 03:40:48 +0200

    Computer Selection

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ const choices = {
   spock: { name: 'Spock', defeats: ['scissors', 'rock'] },
 };
 
+let computerChoice = "";
+
 // All Selected Icons
 function resetSelected() {
   allGameIcons.forEach((icon) => {
@@ -34,9 +36,61 @@ function resetSelected() {
   });
 }
 
+// Random Computer Choice
+
+function computerRandomChoice() {
+  const computerChoiceNumber = Math.random();
+  if (computerChoiceNumber < 0.2){
+    computerChoice = 'rock';
+  }else if (computerChoiceNumber <= 0.4){
+    computerChoice = 'paper';
+  }else if (computerChoiceNumber <= 0.6){
+    computerChoice = 'scissors';
+  }else if (computerChoiceNumber <= 0.8){
+    computerChoice = 'lizard';
+  }else {
+    computerChoice = 'spock';
+  }
+}
+
+// Add selected styling and computer choice
+
+function displayComputerChoice() {
+  // Add 'selected' styling and update player choice
+  switch (computerChoice) {
+    case 'rock':
+      computerRock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Rock';
+      break;
+    case 'paper':
+      computerPaper.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Paper';
+      break;
+    case 'scissors':
+      computerScissors.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Scissors';
+      break;
+    case 'lizard':
+      computerLizard.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Lizard';
+      break;
+    case 'spock':
+      computerSpock.classList.add('selected');
+      computerChoiceEl.textContent = ' --- Spock';
+      break;
+  }
+}
+
+// Call Functions to process turn
+function checkResult() {
+  resetSelected();
+  computerRandomChoice();
+  displayComputerChoice();
+}
+
 // Passing Player selection value and styling icons
 function select(playerChoice) {
-  resetSelected();
+  checkResult();
   // Add 'selected' styling and update player choice
   switch (playerChoice) {
       case 'rock':
